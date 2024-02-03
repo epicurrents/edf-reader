bfb287e114c69e08be363c3019802c5e6b58f26f
sam-19 <26563023+sam-19@users.noreply.github.com>
2024-02-03 12:42:24 +0200

    Next trial version
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- src/edf/EdfWorkerSubstitute.ts
+++ src/edf/EdfWorkerSubstitute.ts
@@ -5,11 +5,11 @@
  * @license    Apache-2.0
  */
 
-import { type BiosignalHeaderRecord, type ConfigChannelFilter } from '@epicurrents/core/dist/types'
 import EdfFileReader from './EdfFileReader'
-import { type EdfHeader } from '../types'
-import { Log } from 'scoped-ts-log'
 import { ServiceWorkerSubstitute } from '@epicurrents/core'
+import { validateCommissionProps } from '@epicurrents/core/dist/util'
+import { type ConfigChannelFilter } from '@epicurrents/core/dist/types'
+import { Log } from 'scoped-ts-log'
 
 const SCOPE = 'EdfWorkerSubstitute'
 
@@ -23,8 +23,20 @@ export default class EdfWorkerSubstitute extends ServiceWorkerSubstitute {
         Log.debug(`Received message with action ${action}.`, SCOPE)
         if (action === 'get-signals') {
             // Extract job parameters.
-            const range = message.data.range as number[]
-            const config = message.data.config as ConfigChannelFilter
+            const data = validateCommissionProps(
+                message.data,
+                {
+                    config: Object,
+                    range: [Number, Number],
+                },
+                true,
+                this.returnMessage
+            )
+            if (!data) {
+                return
+            }
+            const range = data.range as number[]
+            const config = data.config as ConfigChannelFilter
             try {
                 const sigs = this._reader.getSignals(range, config)
                 const annos = this._reader.getAnnotations(range)
@@ -55,18 +67,21 @@ export default class EdfWorkerSubstitute extends ServiceWorkerSubstitute {
                 })
             }
         } else if (action === 'setup-study') {
-            // Check EDF header.
-            const formatHeader = message.data.formatHeader as EdfHeader | undefined
-            if (!formatHeader) {
-                Log.error(`Commission is missing a format-specific header.`, SCOPE)
-                this.returnMessage({
-                    action: action,
-                    success: false,
-                    rn: message.data.rn,
-                })
+            const data = validateCommissionProps(
+                message.data,
+                {
+                    formatHeader: Object,
+                    header: Object,
+                    url: String,
+                },
+                true,
+                this.returnMessage
+            )
+            if (!data) {
                 return
             }
-            const reserved = formatHeader.reserved as string | undefined
+            // Check EDF header.
+            const reserved = data.formatHeader.reserved as string | undefined
             if (!reserved?.startsWith('EDF')) {
                 Log.error(`Format-specific header is not an EDF-compatible format.`, SCOPE)
                 this.returnMessage({
@@ -76,27 +91,7 @@ export default class EdfWorkerSubstitute extends ServiceWorkerSubstitute {
                 })
                 return
             }
-            const header = message.data.header as BiosignalHeaderRecord | undefined
-            if (!header) {
-                Log.error(`Commission is missing a generic biosignal header.`, SCOPE)
-                this.returnMessage({
-                    action: action,
-                    success: false,
-                    rn: message.data.rn,
-                })
-                return
-            }
-            const url = message.data.url as string | undefined
-            if (!url) {
-                Log.error(`Commission is missing a source URL.`, SCOPE)
-                this.returnMessage({
-                    action: action,
-                    success: false,
-                    rn: message.data.rn,
-                })
-                return
-            }
-            this._reader.setupStudy(header, formatHeader, url).then(result => {
+            this._reader.setupStudy(data.header, data.formatHeader, data.url).then(result => {
                 if (result) {
                     this.returnMessage({
                         action: action,
